fcba5f6ea5cf8201d9e22d60bff90822b74ab389
Nicholas Long <nicholas.long@nrel.gov>
2013-12-10 07:32:18 -0700

    updating spec tests for models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,15 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'rspec'
 require 'openstudio-analysis'
 
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
 
 
