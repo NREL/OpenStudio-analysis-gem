0bdd0f6bd9572945bda4d1fd12fdb44af862b7c3
Nicholas Long <nicholas.long@nrel.gov>
2014-06-11 08:21:10 -0600

    fix specs and add 0.3.0 test for objective functions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'rspec'
 require 'openstudio-analysis'
+require 'rspec'
 
 RSpec.configure do |config|
   # Use color in STDOUT
