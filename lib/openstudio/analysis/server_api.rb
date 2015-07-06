fad14a935349b6502465e05460a01967070b4b7f
rHorsey <horsey.henry@gmail.com>
2015-07-06 12:56:07 -0600

    adding batch run queuing

--- lib/openstudio/analysis/server_api.rb
+++ lib/openstudio/analysis/server_api.rb
@@ -826,7 +826,7 @@ module OpenStudio
 
         # If the analysis is a staged analysis, then go ahead and run batch run because there is
         # no explicit way to tell the system to do it
-        if %w(lhs preflight single_run repeat_run doe diag).include? analysis_type
+        if %w(lhs preflight single_run repeat_run doe diag baseline_perturbation).include? analysis_type
           run_options = {
             analysis_action: 'start',
             without_delay: false,
