42d7435c1d6bd0faa57dba37141e0374235483f7
Ry Horsey <henry.horsey@nrel.gov>
2015-07-09 11:01:29 -0600

    Fixing tabs in analysis.rb

--- lib/openstudio/analysis.rb
+++ lib/openstudio/analysis.rb
@@ -27,13 +27,13 @@ module OpenStudio
       excel.process
       options = {
         :os_server_version => excel.settings['openstudio_server_version'],
-	   :server_instance_type => excel.settings['server_instance_type'],
-	   :worker_instance_type => excel.settings['worker_instance_type'],
-           :worker_node_number => excel.settings['worker_nodes'].to_i,
-	   :user_id => excel.settings['user_id'],
-	   :aws_tags => excel.aws_tags,
-	   :analysis_type => excel.analyses.first.analysis_type,
-           :cluster_name => excel.cluster_name
+	:server_instance_type => excel.settings['server_instance_type'],
+	:worker_instance_type => excel.settings['worker_instance_type'],
+        :worker_node_number => excel.settings['worker_nodes'].to_i,
+	:user_id => excel.settings['user_id'],
+	:aws_tags => excel.aws_tags,
+	:analysis_type => excel.analyses.first.analysis_type,
+        :cluster_name => excel.cluster_name
       }
 
       options
