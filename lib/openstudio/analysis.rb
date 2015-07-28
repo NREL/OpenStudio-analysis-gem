c135af6827d733c2cd548780e8e9501bc2328815
Horsey <hhorsey@hhorsey-27636s.local>
2015-07-28 16:44:09 -0600

    Adding other changes

--- lib/openstudio/analysis.rb
+++ lib/openstudio/analysis.rb
@@ -21,22 +21,39 @@ module OpenStudio
       excel.analyses
     end
 
+    # Load an set of batch datapoints from a csv. This will create a analysis
+    # of type 'batch_datapoints' which requires 'batch_run'
+    def self.from_csv(filename)
+      csv = OpenStudio::Analysis::Translator::CSV.new(filename)
+      csv.process
+      csv.analysis
+    end
+
     #Retrieve aws instance options from a project. This will return a hash
     def self.aws_instance_options(filename)
-      excel = OpenStudio::Analysis::Translator::Excel.new(filename)
-      excel.process
-      options = {
-        :os_server_version => excel.settings['openstudio_server_version'],
-	:server_instance_type => excel.settings['server_instance_type'],
-	:worker_instance_type => excel.settings['worker_instance_type'],
-        :worker_node_number => excel.settings['worker_nodes'].to_i,
-	:user_id => excel.settings['user_id'],
-	:aws_tags => excel.aws_tags,
-	:analysis_type => excel.analyses.first.analysis_type,
-        :cluster_name => excel.cluster_name
-      }
+      if File.extname(filename) == ".xlsx"
+        excel = OpenStudio::Analysis::Translator::Excel.new(filename)
+        excel.process
+        options = {
+          :os_server_version => excel.settings['openstudio_server_version'],
+          :server_instance_type => excel.settings['server_instance_type'],
+          :worker_instance_type => excel.settings['worker_instance_type'],
+          :worker_node_number => excel.settings['worker_nodes'].to_i,
+          :user_id => excel.settings['user_id'],
+          :aws_tags => excel.aws_tags,
+          :analysis_type => excel.analyses.first.analysis_type,
+          :cluster_name => excel.cluster_name
+        }
+      elsif File.extname(filename) == ".csv"
+        csv = OpenStudio::Analysis::Translator::CSV.new(filename)
+        csv.process
+        options = csv.settings
+      else
+        fail 'Invalid file extension'
+      end
+      
+      return options
 
-      options
     end
   end
 end
