308257cbc8b8617b74f61177c4630cc6f33bffe9
brianlball <brian.ball@nrel.gov>
2020-07-24 17:36:55 -0600

    try no seed,weatherfile

--- lib/openstudio/analysis/translator/workflow.rb
+++ lib/openstudio/analysis/translator/workflow.rb
@@ -68,11 +68,11 @@ module OpenStudio
           @measure_paths = options[:measure_paths] ? options[:measure_paths] : []
 
           # Initialize static inputs from the OSA
-          @seed_file = File.basename(@osa[:seed][:path])
+          @osa[:seed][:path] ? @seed_file = File.basename(@osa[:seed][:path]) : @seed_file = ''
           if @options[:seed]
             @seed_file = @options[:seed]
           end
-          @weather_file = File.basename(@osa[:weather_file][:path])
+          @osa[:weather_file][:path] ? @weather_file = File.basename(@osa[:weather_file][:path]) : @weather_file = ''
           @osa_id = @osa[:_id]
           @steps = []
           @osa[:problem][:workflow].each_with_index do |step, i|
