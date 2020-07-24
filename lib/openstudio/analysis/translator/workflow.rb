610fa053382030cb361cc6131ed4b121b1a43632
brianlball <brian.ball@nrel.gov>
2020-07-24 17:54:58 -0600

    check nil?

--- lib/openstudio/analysis/translator/workflow.rb
+++ lib/openstudio/analysis/translator/workflow.rb
@@ -68,11 +68,11 @@ module OpenStudio
           @measure_paths = options[:measure_paths] ? options[:measure_paths] : []
 
           # Initialize static inputs from the OSA
-          @osa[:seed][:path] ? @seed_file = File.basename(@osa[:seed][:path]) : @seed_file = ''
+          !@osa[:seed].nil? ? @seed_file = File.basename(@osa[:seed][:path]) : @seed_file = ''
           if @options[:seed]
             @seed_file = @options[:seed]
           end
-          @osa[:weather_file][:path] ? @weather_file = File.basename(@osa[:weather_file][:path]) : @weather_file = ''
+          !@osa[:weather_file].nil? ? @weather_file = File.basename(@osa[:weather_file][:path]) : @weather_file = ''
           @osa_id = @osa[:_id]
           @steps = []
           @osa[:problem][:workflow].each_with_index do |step, i|
